ab3baf91fcd31a86bb19097f87d4821200927043
ali mahmoodi <alimahmudi.alami@gmail.com>
2022-11-19 15:05:02 +0330

    switch component

---
+++ src/baseComponent/src/stories/base/switch/index.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, Story } from "@storybook/react/types-6-0";
+import { useRef, useState } from "react";
+import { Switch, SwitchProps } from "../../../molecules";
+import { StoryContainer } from "../../container";
+export default {
+  title: "switch",
+  component: Switch,
+} as Meta<SwitchProps>;
+
+const Template: Story<SwitchProps> = () => {
+  const [isCheck, setIsCheck] = useState<boolean>(false);
+  const ref = useRef<HTMLInputElement>(null);
+  const handleOnClick = (checked: boolean) => {
+    console.log({ checked });
+    setIsCheck(checked);
+  };
+  const clickToFocus = () => {
+    ref.current?.focus();
+  };
+
+  return (
+    <StoryContainer>
+      <Switch ref={ref} onChange={handleOnClick} checked={isCheck} />
+      <div style={{ marginTop: 50 }}>
+        <button onClick={clickToFocus}>click to focus!</button>
+      </div>
+    </StoryContainer>
+  );
+};
+
+export const RadioGroup = Template.bind({});

6820f0c8d6d98b7db2a4164349319bb028b69306
ali mahmoodi <alimahmudi.alami@gmail.com>
2023-03-15 13:56:26 +0330

    tgdsgsdfg

---
+++ types/molecules/radio/context.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="react" />
+export declare type ValueType = string | number | undefined | null;
+export declare type ModeType = "dark" | "light";
+interface RadioContextProps {
+    value: string | number | undefined | null;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    name: string | undefined;
+    mode: ModeType;
+}
+declare const RadioContext: import("react").Context<RadioContextProps>;
+export { RadioContext };

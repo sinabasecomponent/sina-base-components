885570ce612fc268fbc60a97bb4bbbc46e147c7d
ali mahmoodi <alimahmudi.alami@gmail.com>
2023-03-19 13:15:15 +0330

    sdfsdf

--- src/atoms/drawer/style.ts
+++ src/atoms/drawer/style.ts
@@ -3,6 +3,6 @@ import { ColorsType } from "../../theme/context";
 
 export const useStyle = createUseStyles((theme: ColorsType) => ({
   sag: {
-    background: theme.color_primary_1,
+    backgroundColor: theme.color_primary_1,
   },
 }));

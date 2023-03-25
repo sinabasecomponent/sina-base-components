6ed153fed61f5e430c72ee6a07295be01202d9a9
ali mahmoodi <alimahmudi.alami@gmail.com>
2023-03-25 17:53:36 +0430

    cover style

--- src/molecules/cover/index.tsx
+++ src/molecules/cover/index.tsx
@@ -1,17 +1,31 @@
 // import { StartFlagIcon } from 'assets/icons';
+import { useState } from "react";
 import { Text } from "../../atoms";
 import { BaseIcon } from "../../atoms/baseIcon";
-import styles from "./cover.module.scss";
+import { useTheme } from "../../theme/context";
+import { useStyle } from "./style";
 
 const Cover = () => {
+  const classes = useStyle();
+  const { color_primary_2, color_secondary_1 } = useTheme();
+  const [isHover, setIsHover] = useState(false);
+  const handleHover = () => {
+    setIsHover((prev) => !prev);
+  };
   return (
-    <div className={styles["cover"]}>
-      <div className={styles["text-box"]}>
+    <div className={classes["cover"]}>
+      <div className={classes["title"]}>
         <BaseIcon
-          size={{ height: 30, width: 30 }}
+          onMouseEnter={handleHover}
+          onMouseLeave={handleHover}
+          size={{ height: 20, width: 20 }}
           name="Title-Bar-Icon-_-Exit"
+          color={isHover ? color_secondary_1 : color_primary_2}
+          wrapperStyle={{ cursor: "pointer" }}
         />
-        <Text size={16}> Hi! LOAD OR CREATE A PROJECT AND START!</Text>
+        <Text size={16} color={color_primary_2}>
+          Hi! LOAD OR CREATE A PROJECT AND START!
+        </Text>
       </div>
     </div>
   );

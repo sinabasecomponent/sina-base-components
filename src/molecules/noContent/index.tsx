4a17e22281ec063745a74695cb9a8f304c54c47b
ali mahmoodi <alimahmudi.alami@gmail.com>
2023-03-25 18:33:20 +0430

    no content sytle

--- src/molecules/noContent/index.tsx
+++ src/molecules/noContent/index.tsx
@@ -1,15 +1,16 @@
 import { BaseIcon, Text } from "../../atoms";
 import { Colors } from "../../colors";
 import { pxToVh } from "../../utils/convertUnit";
-import styles from "./noContent.module.scss";
+import { useStyles } from "./style";
 
-interface NoContentProps {
+export interface NoContentProps {
   text: string;
 }
 
 const NoContent = ({ text }: NoContentProps) => {
+  const classes = useStyles();
   return (
-    <div className={styles["no-content"]}>
+    <div className={classes["noContent"]}>
       <BaseIcon
         wrapperStyle={{ marginBottom: 10 }}
         size={{ height: pxToVh(107), width: pxToVh(91) }}
